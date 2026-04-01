4208979aaafdd54f2a22a45e18bac2eb45c15c54
hsgit19 <harinilingampelli@gmail.com>
2026-04-01 16:36:27 -0400

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]):
+        temp = set()
+        for num in nums:
+           if num in temp:
+             return True
+           else:
+             temp.add(num)
+
+        return False                
+        #time complexity o(n)
